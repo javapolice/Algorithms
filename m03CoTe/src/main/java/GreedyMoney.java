248ff513a2aa514dd6cfe2dd87d4e1dc80877335
Dongmin <ehdals0116@gmail.com>
2021-03-17 01:59:43 +0900

    greedyAL Money Example

---
+++ m03CoTe/src/main/java/GreedyMoney.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class GreedyMoney {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int money = Integer.parseInt(st.nextToken());
+        int coin[] = {500, 100, 50, 10};
+        int count = 0;
+        for(int i = 0; i < coin.length; i++){
+            count += money / coin[i];
+            System.out.println(coin[i] + ":" + money/coin[i]);
+            money = money % coin[i];
+        }
+        System.out.println(count);
+    }
+}
