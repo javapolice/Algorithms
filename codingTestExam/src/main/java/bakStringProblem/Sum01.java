30a41736f8e5c01aa5ac6d0d617bd809eb138164
KDM <ehdals0116@gmail.com>
2021-03-29 20:45:21 +0900

    Bak String commit

---
+++ codingTestExam/src/main/java/bakStringProblem/Sum01.java
@@ -0,0 +1,12 @@
+package bakStringProblem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sum01 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
