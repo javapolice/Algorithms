131c22323d2a85cc24f7cc420e81cf72d7160012
KDM <ehdals0116@gmail.com>
2022-09-28 17:33:38 +0900

    first commit

---
+++ src/test/java/com/kdm/baekjoon/BaekjoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdm.baekjoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekjoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
