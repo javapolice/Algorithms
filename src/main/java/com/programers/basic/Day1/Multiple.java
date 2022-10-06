8b347d7408127ccb94cc2650c4d9199f0b18e51b
KDM <ehdals0116@gmail.com>
2022-10-06 17:11:50 +0900

    프로그래머스 기본 문제 커밋

---
+++ src/main/java/com/programers/basic/Day1/Multiple.java
@@ -0,0 +1,18 @@
+package com.programers.basic.Day1;
+
+import java.util.Scanner;
+
+public class Multiple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int result = solution(num1, num2);
+        System.out.println(result);
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
